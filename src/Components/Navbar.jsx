8bb21ac757ef644197c28b60e345bb79079c8d72
Atif shahwaiz <atifshahwaiz023@gmail.com>
2026-09-22 14:00:57 +0500

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
           <a href="#contact">Contact</a>
         </nav>
         <a 
-          href="/Atiff-Shahwaiz-CV.pdf" 
+          href="/Atiff%20shahwaiz%20DV.pdf" 
           download="Atiff_Shahwaiz_CV.pdf" 
           className="btn nav-cta"
         >
